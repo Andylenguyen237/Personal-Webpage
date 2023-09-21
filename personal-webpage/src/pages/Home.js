5d7e82fae2f8e0a5020912c04f4048a2290ca848
Andylenguyen237 <lenguyenlinhdan02@gmail.com>
2023-09-21 12:18:52 +1000

    about section

--- personal-webpage/src/pages/Home.js
+++ personal-webpage/src/pages/Home.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import ParticlesComponent from '../components/Particles'
+import Footer from '../components/Footer'
+import andy from '../images/andy1.png'
+import Typewriter from 'typewriter-effect'
 
 function home() {
+
+  const introduction = ""
   return (
     <>
-    <div className='App'>
-        <header className='App-header'>
-            Andy's Portfolio
-        </header>
+    <div className='introduction'>
+        <h1>
+            Hey there, I'm Andy 
+        </h1>
         <ParticlesComponent id="tsparticles" />
 
     {/* About section */}
     <div className='about-section'>
-    <h2 id="about">About Me</h2>
-    <p>
-      I'm Nguyen Linh Dan Le (Andy Le), I'm currently a last year Computer Science students stuyding at the University of Melbourne
-    </p>
+      <h2 id="about"></h2>
+      
+      <div className='about-container'>
+        <div className='about-container-text'>
+        <Typewriter
+            options={{
+              autoStart:true,
+              loop:true,
+              delay:80,
+              strings: ["I'm a Software Developer"]
+            }}
+          />
+          <h3>
+            I'm currently a last year Computer Science student studying at the University of Melbourne. As a student I have rich working experiences as an intern as a Software/Data Engineer at Walter and Eliza Hall Institute of Medical Research and Software Engineer at ITG
+          </h3>
+        </div>
+        <img src={andy}></img>
+      </div>
     </div>
 
     <div className='profi-section'>
@@ -24,7 +43,9 @@ function home() {
       Java
     </p>
     </div>
-
+    
+    <div></div>
+    <Footer></Footer>
 
     </div>
     </>
